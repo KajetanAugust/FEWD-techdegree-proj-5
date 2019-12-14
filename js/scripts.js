93858f23cae96e431b840a8ea6fcfc13d9291128
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2019-12-14 21:19:44 +0100

    styling, added lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ $('input').show();
 
 
 $('input').keyup(function() {
-    
+    const $search = $('input').val().toLowerCase();
+    if($search !== $('div a.attr(data-caption)').toLowerCase())  {
+        $(this).hide();
+    }
 
 });

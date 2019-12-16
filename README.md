3d05d03ea0b4efa495ed3f5188c7171a1fbb296f
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2019-12-16 11:34:01 +0100

    comments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# FEWD-techdegree-proj-5
- FEWD techdegree unit 5 project.
+This is my submission for Unit 3 project. It's a gallery page. It includes jQuery plugin for the lightbox (from fancybox http://fancyapps.com/fancybox/) and the original jquery code for search function with keyup event listener.
+
+The jquery search has keyup event listener which on activation 
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 $('input').show();
 
-// DESKTOP SEARCH
+// SEARCH
 
-$('.desktop-search').on('keyup',function() {
-    const $searchVal = $('.desktop-search').val().toLowerCase();
+$('.search-bar').on('keyup',function() {
+    // The search input value is collected and transformed to lower case.
+    const $searchVal = $('.search-bar').val().toLowerCase();
+    // The data caption value is targetted
     const $descriptions =$('a[data-caption]');
+    // Each function loops through all data caption attribute values
     $descriptions.each(function(i){
         const $attrDataCaption = $(this).attr('data-caption').toLowerCase().replace('<br>', '');
+    //If function is checking if search value exist inside any of data caption attributes 
+    //It hides this photos which are not matched.
         if($attrDataCaption.includes($searchVal)){
             $(this).show();
         }else {
@@ -14,20 +19,4 @@ $('.desktop-search').on('keyup',function() {
         }
     });
 });
-
-// MOBILE SEARCH
-
-$('.mobile-search-box').keyup(function() {
-    const $searchVal = $('.mobile-search-box').val().toLowerCase();
-    const $descriptions =$('a[data-caption]');
-    $descriptions.each(function(i){
-        const $attrDataCaption = $(this).attr('data-caption').toLowerCase().replace('<br>', '');
-        if($attrDataCaption.includes($searchVal)){
-            $(this).show();
-        }else {
-            $(this).hide()
-        }
-    });
-});
-
 

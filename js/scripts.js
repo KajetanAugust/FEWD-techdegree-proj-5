8def3db8ec94b35551559269e6b1a91e501d06d5
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-01-15 20:30:50 +0100

    js changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // SEARCH
 
 $('.search-bar').on('keyup',function() {
-    // The search input value is collected and transformed to lower case.
-    const $searchVal = $('.search-bar').val().toLowerCase();
-    // The data caption value is targetted
-    const $descriptions =$('a[data-caption]');
-    // Each function loops through all data caption attribute values
-    $descriptions.each(function(i){
+   
+    const $searchVal = $('.search-bar').val().toLowerCase();  // The search input value is collected and transformed to lower case.
+    
+    const $descriptions =$('a[data-caption]'); // The data caption value is targetted
+    
+    $descriptions.each(function(i){ // Each function loops through all data caption attribute values
         const $attrDataCaption = $(this).attr('data-caption').toLowerCase().replace('<br>', '');
     //If function is checking if search value exist inside any of data caption attributes 
     //Data caption attribute values are transformed to lower case and the <br> tag is removed
